704adcd7791615116736b94a4ce733c6aa08a0ec
uroatienza <l.atienza@irri.org>
2013-11-29 16:13:15 +0800

    Update default.php

--- modules/mod_zooitempro/tmpl/default.php
+++ modules/mod_zooitempro/tmpl/default.php
@@ -1,1 +1,38 @@
-asdasd
+<?php
+/**
+* @package		ZOOitem Pro
+* @author    	ZOOlanders http://www.zoolanders.com
+* @copyright 	Copyright (C) JOOlanders SL
+* @license   	http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+*/
+
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+// include css
+$zoo->document->addStylesheet('mod_zooitempro:tmpl/default/style.css');
+
+// get items
+$items = mod_zooitemproHelper::getItems($params);
+?>
+
+<?php if (!empty($items)) : ?>
+
+<div style="width:100%; padding: 10px;">
+	<?php $i = 1; foreach ($items as $item) : ?>    
+	<div class="zoo-itempro-default"><?php echo $renderer->render('item.'.$renderer_layout, compact('item', 'params')); ?></div>
+    
+    
+    <?php if ($i % 2 == 0) : ?>
+    <div style="clear:both;"></div>
+    <?php endif; ?>
+    
+    
+	<?php $i++; endforeach; ?>
+</div>
+
+
+<?php else : ?>
+<?php echo JText::_('MOD_ZOOITEMPRO_NO_ITEMS_FOUND'); ?>
+<?php endif; ?>
