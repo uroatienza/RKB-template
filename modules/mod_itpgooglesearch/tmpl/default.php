cb90efcea43a8b5f8be3f26bbc168049219fe0f9
uroatienza <l.atienza@irri.org>
2014-02-06 14:02:09 +0800

    Create default.php

---
+++ modules/mod_itpgooglesearch/tmpl/default.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package      ITPGoogleSearch
+ * @subpackage   Modules
+ * @author       Todor Iliev
+ * @copyright    Copyright (C) 2013 Todor Iliev <todor.iliev@itprism.com>. All rights reserved.
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ */
+ 
+defined('_JEXEC') or die; ?>
+<div class="itp-gs<?php echo $moduleclass_sfx;?>">
+	<form action="<?php echo JRoute::_('index.php?option=com_itpgooglesearch&view=search'); ?>" method="get" accept-charset="utf-8" class="searchbox">
+        <input name="gsquery" type="text" class="inputbox" placeholder="<?php echo JText::_("MOD_ITPGOOGLESEARCH_SEARCH_FOR"); ?>" value="<?php echo $phrase; ?>" />
+        <?php if($params->get("searchButton")) {?>
+        <input type="submit" class="btn" value="<?php echo JText::_("MOD_ITPGOOGLESEARCH_SEARCH"); ?>" />
+        <?php }?>
+    </form>
+</div>
