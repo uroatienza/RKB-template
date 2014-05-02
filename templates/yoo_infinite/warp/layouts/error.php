ed9f16df26a5146c0a98917fae25c3e6757f03ec
uroatienza <l.atienza@irri.org>
2014-05-02 08:17:59 +0800

    Create error.php

---
+++ templates/yoo_infinite/warp/layouts/error.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>RKB Redirecting... Please wait...</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<script language="JavaScript">
+<!--
+function redirect(URLStr) { location = URLStr; }
+//-->
+</script> 
+<body onLoad="redirect('http://www.knowledgebank.irri.org/404-page-not-found')">
+<div align="center"><span class="style2">Redirecting...</span>
+</div>
+</body>
+</html>
